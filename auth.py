672b07a17151825b44ec5af36b88f27563efe2fd
jeor0980 <jesusotovar@hotmail.com>
2020-01-27 14:17:15 -0700

    Adding sha1auth class to auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 from eve import Eve
-from eve.auth import TokenAuth
+from eve.auth import BasicAuth, TokenAuth
+from werkzeug.security import check_password_hash
 from flask import current_app as app
 
 
@@ -15,3 +16,12 @@ class WalnutTokenAuth(TokenAuth):
         # use Eve's own db driver; no additional connections/resources are used
         accounts = app.data.driver.db['accounts']
         return accounts.find_one({'token': token})
+
+
+class Sha1Auth(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource, method):
+        # use Eve's own db driver; no additional connections/resources are used
+        accounts = app.data.driver.db['accounts']
+        account = accounts.find_one({'username': username})
+        return account and \
+            check_password_hash(account['password'], password)
