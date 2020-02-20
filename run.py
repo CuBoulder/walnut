844b193fb33429fec826ced9629154a46c67f98f
jeor0980 <jesusotovar@hotmail.com>
2020-02-20 15:59:19 -0700

    renaming application in run.py

--- run.py
+++ run.py
@@ -4,9 +4,9 @@
 from eve import Eve
 from auth import add_token, hash_password
 
-APP = Eve()
-APP.on_insert_accounts += add_token
-APP.on_insert_accounts += hash_password
+WALNUT = Eve()
+WALNUT.on_insert_accounts += add_token
+WALNUT.on_insert_accounts += hash_password
 
 if __name__ == "__main__":
-    APP.run()
+    WALNUT.run()
