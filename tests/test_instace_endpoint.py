caa977600ac3ac45fc08ec1233d73b1a8c387d7c
jeor0980 <jesusotovar@hotmail.com>
2020-06-04 10:39:47 -0600

    Creating tests directory and updating .travis to start walnut locally

---
+++ tests/test_instace_endpoint.py
@@ -0,0 +1,15 @@
+"""Travis-CI GET Tests."""
+import requests
+
+
+def test_get_instance_endpoint():
+    """Test that /instance endpoint is working and returns a 200 response."""
+    url = "http://127.0.0.1:5000/instance"
+    # convert dict to json by json.dumps() for body data.
+    response = requests.get(url)
+
+    # Validate response headers and body contents, e.g. status code.
+    assert response.status_code == 200
+
+    # print response full body as text
+    print(response.text)
